882822716e3a5642e6de2600d9768ee20154f552
Dobre Aurel-Ninel <101126905+AurelDobre20@users.noreply.github.com>
2022-12-25 23:59:41 +0200

    Done

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,60 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, number, noRem, no3, no4) {
+    
+    if(noRem==null){
+        console.log('da1')
+        array.splice(number-1, 1);
+        return array;
+    }
+    if(typeof noRem =='number' && array.includes(number) && no3==null &&no4==null&&typeof number!='string'){
+       
+       let i = noRem-1
+       let count=0;
+        while(count<number-1){
+            
+            array.splice(noRem-1,1);
+            count++;
+            
+         }
+        console.log(array);
+        console.log('da')
+        
+        return array;
+    }
+    if(!(array.includes(noRem))){
+        return array;
+    }
+    if(!(array.includes(number))){
+        array.splice(noRem-1,1);
+        console.log(array)
+        return array;
+    }
+    if(number!=null &&noRem!=null &&no3!=null &&no4!=null ){
+        count=4;
+        while(count>0){
+            count--;
+            array.splice(0,1);
+        }
+        console.log(array)
+        return array;
+    }
+    if(typeof number=='string'){
+        if(array.includes(number)){
+            let i = array.indexOf(number);
+            array.splice(i,1);
+            i = array.indexOf(noRem);
+            array.splice(i,1);
+            return array;
+        }
+        else{
+            array.splice(noRem,1);
+        }
+    }
+
+    
 
 };
+
+removeFromArray(["hey", 2, 3, "ho"], "hey", 3);
 
 // Do not edit below this line
 module.exports = removeFromArray;
--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,25 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(number) {
+  if(number % 32 == 0){
+    let b = (number - 32)* 5/9;
+    return b;
+  }else{
+    let a = (number - 32)* 5/9;
+    let a1 = Number(a.toFixed(1));
+    
+    return a1;
+  }
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(number) {
+  if(number % 0 == 0){
+    let b = (number * 9/5 + 32);
+    return b;
+  }else{
+    let a = (number * 9/5 + 32);
+    let a1 = Number(a.toFixed(1));
+    
+    return a1;
+  }
 };
 
 // Do not edit below this line
